6a691f4aafb1fc27368242fdcbf478b67e25335e
ppla.fs <sukrit.nak@gmail.com>
2021-01-02 11:57:14 +0700

    push noti OneSignal android

--- firstFlutter/lib/main.dart
+++ firstFlutter/lib/main.dart
@@ -10,13 +10,54 @@ import 'package:firstFlutter/pages/stacks/ProductStack.dart';
 import 'package:firstFlutter/redux/appReducer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:redux/redux.dart';
 
 String token;
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+//// config
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.init("e48a027a-98a8-4abe-a145-d0274e3b2571", iOSSettings: {
+    OSiOSSettings.autoPrompt: false,
+    OSiOSSettings.inAppLaunchUrl: false
+  });
+  OneSignal.shared
+      .setInFocusDisplayType(OSNotificationDisplayType.notification);
+
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  await OneSignal.shared
+      .promptUserForPushNotificationPermission(fallbackToSettings: true);
+//// config
+
+  OneSignal.shared
+      .setNotificationReceivedHandler((OSNotification notification) {
+    // will be called whenever a notification is received
+    print('noti from server' + notification.jsonRepresentation());
+    print('noti from server' + notification.payload.body);
+    print('noti from server' + notification.payload.title);
+  });
+
+  OneSignal.shared
+      .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+    // will be called whenever a notification is opened/button pressed.
+    print('result');
+    // เปิดหน้าข่าว ที่ ส่งมาจาก OneSignal
+    print(result.notification.payload.additionalData['page']);
+    // ปกติเราเขียนแบบนี้ แต่อันนี้เราไม่มี context ใช้ที่นี่ไม่ได้เลยต้องไปใช้ global key
+    // Navigator.pushNamed(context, routeName)
+
+
+    navigatorKey.currentState.pushNamed(result.notification.payload.additionalData['page']);
+
+  });
+
   SharedPreferences prefs = await SharedPreferences.getInstance();
   token = prefs.getString('token');
 
@@ -41,6 +82,7 @@ class MyApp extends StatelessWidget {
         store: store,
         child: MaterialApp(
           title: 'Flutter pla Demo',
+          navigatorKey: navigatorKey,
           theme: ThemeData(
               // primarySwatch: Colors.brown,
               primaryColor: Colors.cyan[600],
